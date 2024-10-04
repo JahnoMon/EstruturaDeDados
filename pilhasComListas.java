f589fff73d94a1e751023989671a8e32ba98933b
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2024-10-04 00:58:46 -0300

    Update pilhasComListas.java

--- pilhasComListas.java
+++ pilhasComListas.java
@@ -1,66 +1,63 @@
-import java.io.* 
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    
-    public object[] pilha;
-    public int posicaoPilha;
-    
-    public static void Pilha(){
-        this.posicaoPilha = -1;
-        this.pilha = new Object[100];
-    }
-    
-    public static boolean pilhaVazia(){
-        if (this. posicaoPilha == -1) {
-            return true;
-        } else {
-            return false;
+public class HuxleyCode {
+
+    public static class PilhaDeListas {
+        public Stack<List<Integer>> pilha;
+
+        public PilhaDeListas() {
+            this.pilha = new Stack<>();
+        }
+
+        // Método PUSH: adiciona uma lista de inteiros à pilha
+        public void PUSH(List<Integer> lista) {
+            pilha.push(lista);
+        }
+
+        // Método POP: remove e imprime a lista do topo da pilha no formato esperado
+        public void POP() {
+            if (pilha.isEmpty()) {
+                System.out.println("EMPTY STACK");
+            } else {
+                List<Integer> topo = pilha.pop();
+                for (int i = 0; i < topo.size(); i++) {
+                    if (i == topo.size() - 1) {
+                        System.out.print(topo.get(i)); // Último valor sem espaço
+                    } else {
+                        System.out.print(topo.get(i) + " ");
+                    }
+                }
+                System.out.println(); // Nova linha após a impressão
+            }
         }
     }
-    
-    public static int tamanho(){
-        if(this.pilhaVazia){
-            return 0;
-        } else {
-            return this.posicaoPilha + 1;
+
+    public static void main(String[] args) {
+        PilhaDeListas pilha = new PilhaDeListas();
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String comando = sc.nextLine();
+
+            if (comando.equals("PUSH")) {
+                String linhaDeNumeros = sc.nextLine();
+                String[] numeros = linhaDeNumeros.split(" ");
+
+                // Converter os números para uma lista de inteiros
+                List<Integer> lista = new ArrayList<>();
+                for (String numero : numeros) {
+                    lista.add(Integer.parseInt(numero));
+                }
+
+                // Realizar o PUSH
+                pilha.PUSH(lista);
+            } else if (comando.equals("POP")) {
+                // Realizar o POP
+                pilha.POP();
+            }
         }
+
+        sc.close();
     }
-    
-    public static void PUSH(){
-        if(this.posicaoPilha < this.pilha.length - 1){
-            this.pilha[++posicaoPilha] = valor;
-        }
-    }
-    
-    public Object POP(){
-        if(pilhaVazia()){
-            return null;
-        } else {
-            return this.pilha[this.posicaoPilha --];
-        }
-    }
-    
-	public static void main(String[] args) {
-	    
-	    Pilha p = new Pilha();
-	    Scanner sc = new Scanner(System.in);
-	    int pops = 0;
-	    
-	    do{
-	        String input = sc.nextLine();
-	        switch{
-	            case (input == "POP"):
-	                System.out.println();
-	                pops++;
-	            case (input == "PUSH"):
-	                int[] numeros = sc.nextLine();
-	                for(int n : numeros){
-	                    
-	                }
-	                
-	        }
-	    }while(pop < 3);
-		
-	}
 }
